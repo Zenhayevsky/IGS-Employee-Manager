f4cdebdd6c6149e2065300d0dea9cb26500c537e
Zenhayevsky <74720973+Zenhayevsky@users.noreply.github.com>
2023-04-06 19:34:27 -0300

    correcao
    correcao caompo department

--- employeesmanagement/employeesmanagement/employeeslist/models.py
+++ employeesmanagement/employeesmanagement/employeeslist/models.py
@@ -4,4 +4,7 @@ from django.db import models
 class employee(models.Model):
     name = models.CharField(max_length=255)
     email = models.CharField(max_length=255)
-    departamento = models.CharField(max_length=255)
+    department = models.CharField(max_length=255)
+
+    class Meta: 
+        db_table = 'employeesM'
