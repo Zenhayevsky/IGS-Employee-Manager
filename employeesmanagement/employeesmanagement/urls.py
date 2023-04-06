a1a4e6f0b9ceddc08e32e5894a9f06d972669830
Zenhayevsky <74720973+Zenhayevsky@users.noreply.github.com>
2023-04-06 18:40:57 -0300

    validacoes de cada metodo
    calidacoes e nova pagina de admin

--- employeesmanagement/employeesmanagement/urls.py
+++ employeesmanagement/employeesmanagement/urls.py
@@ -14,10 +14,16 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from employeesmanagement.employeeslist.views import employeeslist
+from django.urls import path, include
+from employeesmanagement.employeeslist.views import EmployeesListViewSet
+from rest_framework import routers
+
+
+router = routers.DefaultRouter()
+router.register(r'employeeslist', EmployeesListViewSet)
+
 
 urlpatterns = [
+    path('', include(router.urls)),
     path('admin/', admin.site.urls),
-    path('employeeslist/', employeeslist)
 ]
