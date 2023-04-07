f0e79939c720444b9f6a5bbdf4789d346d6b56df
Zenhayevsky <74720973+Zenhayevsky@users.noreply.github.com>
2023-04-06 21:44:15 -0300

    novo model adicionado
    novoi model e ajustes de delete

--- employeesmanagement/employeesmanagement/employeeslist/admin.py
+++ employeesmanagement/employeesmanagement/employeeslist/admin.py
@@ -1,10 +1,13 @@
 from django.contrib import admin
-from employeesmanagement.employeeslist.models import employee
+from employeesmanagement.employeeslist.models import employee, Department
 
 
 class EmployeeAdmmin(admin.ModelAdmin):
     list_display = ('name', 'department')
     list_filter = ('department',)
 
+class DepartmentAdmmin(admin.ModelAdmin):
+    list_display = ('namedepartment',)
 
 admin.site.register(employee, EmployeeAdmmin)
+admin.site.register(Department, DepartmentAdmmin)
--- employeesmanagement/employeesmanagement/employeeslist/models.py
+++ employeesmanagement/employeesmanagement/employeeslist/models.py
@@ -4,10 +4,17 @@ from django.db import models
 class employee(models.Model):
     name = models.CharField(max_length=255)
     email = models.CharField(max_length=255)
-    department = models.CharField(max_length=255)
+    department = models.OneToOneField('Department', on_delete=models.SET('6'))
+
 
     class Meta: 
-        db_table = 'employeesM'
+        db_table = 'employeesMn'
 
     def __str__(self):
         return self.name
+    
+class Department(models.Model): 
+    namedepartment = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.namedepartment
